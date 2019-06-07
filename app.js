5fd05908865d54aedfe76791dda2e498d4514c4f
unknown <sharanya.17410@gmail.com>
2019-06-07 17:18:21 -0400

    Temp fix img folder on heroku

--- app.js
+++ app.js
@@ -386,6 +386,7 @@ app.get('/getAlbum/:id', (req, res) => {
     if (!error && response.statusCode === 200) {
 
       var images = JSON.parse(body).data.images;
+      console.log(images);
       if(images.length>1){
       for (var i = 0; i < images.length; i++) {
         array.push({ imagelink: images[i].link, imageid: images[i].id });
